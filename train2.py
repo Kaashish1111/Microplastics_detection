d5c8fae2bc7609959ecfc6280e1d3ca625d8dd0c
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2026-05-26 13:21:43 +0530

    17_was_best_guys

--- train2.py
+++ train2.py
@@ -1,22 +1,16 @@
 from ultralytics import YOLO
 import time
-
 # =========================================
 # START TIMER
 # =========================================
-
 start_time = time.time()
-
 # =========================================
 # LOAD MODEL
 # =========================================
-
 model = YOLO("yolov8n-obb.pt")
-
 # =========================================
 # TRAIN MODEL
 # =========================================
-
 model.train(
     data="data2.yaml",
     epochs=100,
@@ -25,33 +19,22 @@ model.train(
     batch=8,
     device="mps"
 )
-
 # =========================================
 # END TIMER
 # =========================================
-
 end_time = time.time()
-
 # =========================================
 # CALCULATE TIME
 # =========================================
-
 total_seconds = end_time - start_time
-
 minutes = total_seconds / 60
-
 hours = minutes / 60
-
 # =========================================
 # PRINT RESULTS
 # =========================================
-
 print("\n===================================")
 print("TRAINING COMPLETED")
 print("===================================")
-
 print(f"\nTotal Time (seconds): {total_seconds:.2f}")
-
 print(f"Total Time (minutes): {minutes:.2f}")
-
 print(f"Total Time (hours): {hours:.2f}")
